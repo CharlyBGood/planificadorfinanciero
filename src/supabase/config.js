0d7735bf6142c6a4d3243a6259670abbb2b85b07
charlybgood__x <bonavita.carlos@gmail.com>
2025-03-11 15:50:05 -0300

    hardcode env variables temporarily

--- src/supabase/config.js
+++ src/supabase/config.js
@@ -1,12 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://buyyejpwbglqgxncrkam.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1eXllanB3YmdscWd4bmNya2FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2MzAxNjksImV4cCI6MjA1NzIwNjE2OX0.4K0zS54A5FNWizr2i5sjXJFeOz06Iywz3vz_LbcCLP8"
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    redirectTo: import.meta.env.DEV
-      ? 'https://localhost:5173'
-      : 'https://expence-tracker-theta.vercel.app',
-  },
-});
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
