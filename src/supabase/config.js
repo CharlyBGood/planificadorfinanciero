69af4fdcded8a39d0dca8862e284a7e9143b677d
charlybgood__x <bonavita.carlos@gmail.com>
2025-03-11 14:55:21 -0300

    fix env file and config/add redirect config

--- src/supabase/config.js
+++ src/supabase/config.js
@@ -1,9 +1,12 @@
-import { createClient } from "@supabase/supabase-js"
-
-
-
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    redirectTo: import.meta.env.DEV
+      ? 'https://localhost:5173'
+      : 'https://expence-tracker-theta.vercel.app',
+  },
+});
